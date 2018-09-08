ab61095b9795897839042ba76a4075e55559d0d4
gForLee <18990346763@189.cn>
2018-09-09 03:43:04 +0800

    主函数入口

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/*
- * 自己的JS脚步
- * @Author: iceStone
- * @Date:   2015-12-12 10:59:26
- * @Last Modified by:   iceStone
- * @Last Modified time: 2015-12-15 16:23:12
- */
-
 'use strict';
 
 $(function() {
